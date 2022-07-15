18527b9a31f37f3b535e653488237f9283d04caf
nbelchikova <n.belchikova@yandex.ru>
2022-07-15 09:38:43 +0300

    docs:chsnge js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  const firstBracket = ['(', '[', '(', '|',  '1',  '3',  '5',  '7', '8'];
+  const brackets = {
+    [')']: '(',
+    [']']: '[',
+    [')']: '(',
+    ['|']: '|',  
+    ['2']: '1',    
+    ['4']: '3',  
+    ['6']: '5', 
+    ['7']: '7',
+    ['8']: '8',
+    
+  };
+  let stack=[];
+  
+
+
+  
+    for (let i = 0; i < str.length; i++) {
+      
+    
+      if (firstBracket.includes(str[i]) ) {
+        
+        stack.push(str[i]);
+       }
+      else {
+        false
+      }
+        if (brackets[str[i]] === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          false
+        }
+        }
+
+     
+      if (stack.length===0) {
+        return true;
+      }  else {
+        return false;
+      }
+    }
+
+    
+
